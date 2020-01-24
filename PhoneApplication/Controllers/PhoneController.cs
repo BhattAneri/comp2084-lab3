94f7151462fbbb7bc07146e06f1e30e6f2107e7c
BhattAneri <200430090@student.georgianc.on.ca>
2020-01-23 20:41:23 -0500

    LAb 3 Completed

---
+++ PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var PhoneList = new List<Phone>
+            {
+            new Phone { PhoneName="iPhone 11" , 
+                Manufacturer = "Apple",
+                MSRP=1080.50 ,
+                DateReleased= new DateTime(2019,12,08) },
+
+            new Phone {  PhoneName="iPhone 11 Pro",  
+                Manufacturer = "Apple",
+                MSRP=1500.50 ,
+                DateReleased= new DateTime(2019,12,08) },
+
+            new Phone {  PhoneName="Samsung Note 10",
+                Manufacturer = "Samsung",
+                MSRP=965.50 ,
+                DateReleased= new DateTime(2017,10,05) },
+
+            new Phone {  PhoneName="Google Pixel", 
+                Manufacturer = "Google" , 
+                MSRP=852.50 , 
+                DateReleased= new DateTime(2017,10,02)},
+
+            new Phone {  PhoneName="One Plus 7T", 
+                Manufacturer = "One Plus" ,
+                MSRP=750.50 , 
+                DateReleased= new DateTime(2019,06,15)}
+
+            };
+            return View(PhoneList);
+        }
+    }
+}
---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+            public Phone ()
+            {
+               
+                PhoneName = "";
+                Manufacturer="";
+                MSRP = 0;
+                ScreenSize = 0;
+            DateReleased = DateTime.Now;
+            }
+
+        public string PhoneName { get; set; }
+        public int PhoneID { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime DateReleased { get; set; }
+        public double MSRP { get; set; }
+        public float ScreenSize { get; set; }
+
+
+
+    }
+}
